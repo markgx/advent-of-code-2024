3269c8e4fdbad9e20f3bfba4b9a2bc6f51d04246
Mark Guerra <mark@markgx.dev>
2024-12-05 19:37:55 -0800

    fix styling

--- day_02/part1.rb
+++ day_02/part1.rb
@@ -1,3 +1,5 @@
+# frozen_string_literal: true
+
 GRADUAL_MIN_THRESHOLD = 1
 GRADUAL_MAX_THRESHOLD = 3
 
@@ -6,9 +8,7 @@ def sequence_satisfied?(numbers)
     next_num = numbers[i + 1]
     break if next_num.nil?
 
-    unless yield(curr_num, next_num)
-      return false
-    end
+    return false unless yield(curr_num, next_num)
   end
 
   true
@@ -20,18 +20,18 @@ File.open('input.txt', 'r') do |input_file|
   input_file.each_line do |line|
     numbers = line.split(' ').map(&:to_i)
 
-    is_gradually_changing_check = Proc.new do |curr_num, next_num|
+    is_gradually_changing_check = proc do |curr_num, next_num|
       difference = (curr_num - next_num).abs
 
       difference >= GRADUAL_MIN_THRESHOLD && difference <= GRADUAL_MAX_THRESHOLD
     end
 
-    if (
-        sequence_satisfied?(numbers) {|curr_num, next_num| curr_num < next_num } ||
-        sequence_satisfied?(numbers) {|curr_num, next_num| curr_num > next_num }
+    next unless (
+        sequence_satisfied?(numbers) { |curr_num, next_num| curr_num < next_num } ||
+        sequence_satisfied?(numbers) { |curr_num, next_num| curr_num > next_num }
       ) && sequence_satisfied?(numbers, &is_gradually_changing_check)
-      safe_count += 1
-    end
+
+    safe_count += 1
   end
 
   puts "Number of reports safe: #{safe_count}"
